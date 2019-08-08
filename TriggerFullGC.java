36fdde221262daf7f9b91786aa2051a1338fd027
lihongming1 <45524483+lihongming1@users.noreply.github.com>
2019-08-08 18:43:49 +0800

    Update TriggerFullGC.java

--- TriggerFullGC.java
+++ TriggerFullGC.java
@@ -13,49 +13,61 @@ import java.util.List;
  */
 public class TriggerFullGC {
 
-    public static void main(String[] args) throws Exception {
-
-        Process proc = Runtime.getRuntime().exec("jps");
-        List<String> messages = printMessage(proc.getInputStream());
-
-        if (!CollectionUtils.isEmpty(messages)) {
-            for (String message : messages) {
-                if (message.contains("TriggerFullGC")) {
-                    String[] messageArr = message.split(" ");
-                    String pid = messageArr[0];
-                    System.out.println("pid: " + pid);
-                    // 打印日志
-                    Process proc2 = Runtime.getRuntime().exec("jstat -gcutil " + pid);
-                    List<String> messages2 = printMessage(proc2.getInputStream());
-                    if (!CollectionUtils.isEmpty(messages2)) {
-                        for (String msg : messages2) {
-                            System.out.println(msg);
-                        }
-                    }
-                    // 执行命令
-                    String[] exe = {"/bin/sh", "-c", "jmap -histo:live " + pid + " | head -10"};
-                    Process proc3 = Runtime.getRuntime().exec(exe);
-                    System.out.println(proc3);
-                    List<String> messages3 = printMessage(proc3.getInputStream());
-                    if (!CollectionUtils.isEmpty(messages3)) {
-                        for (String msg : messages3) {
-                            System.out.println(msg);
-                        }
-                    }
-                    // 打印日志
-                    Process proc4 = Runtime.getRuntime().exec("jstat -gcutil " + pid);
-                    List<String> messages4 = printMessage(proc4.getInputStream());
-                    if (!CollectionUtils.isEmpty(messages4)) {
-                        for (String msg : messages4) {
-                            System.out.println(msg);
-                        }
-                    }
+    public static void gcName(String name) {
+        try {
+            String pid = null;
+            Process proc = Runtime.getRuntime().exec("jps");
+            List<String> messages = TriggerFullGC.printMessage(proc.getInputStream());
+            for (String str : messages) {
+                if (str.contains(name)) {
+                    String[] strArr = str.split(" ");
+                    pid = strArr[0];
+                    break;
                 }
             }
+            // GC
+            gcPid(pid);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
-    private static List<String> printMessage(final InputStream input) {
+    public static void gcPid(String pid) {
+        try {
+            System.out.println("pid: " + pid);
+            // 打印日志
+            Process proc2 = Runtime.getRuntime().exec("jstat -gcutil " + pid);
+            List<String> messages2 = printMessage(proc2.getInputStream());
+            if (!CollectionUtils.isEmpty(messages2)) {
+                for (String msg : messages2) {
+                    System.out.println(msg);
+                }
+            }
+            // 执行命令
+            String[] exe = {"/bin/sh", "-c", "jmap -histo:live " + pid + " | head -10"};
+            Process proc3 = Runtime.getRuntime().exec(exe);
+            System.out.println(proc3);
+            List<String> messages3 = printMessage(proc3.getInputStream());
+            if (!CollectionUtils.isEmpty(messages3)) {
+                for (String msg : messages3) {
+                    System.out.println(msg);
+                }
+            }
+            // 打印日志
+            Process proc4 = Runtime.getRuntime().exec("jstat -gcutil " + pid);
+            List<String> messages4 = printMessage(proc4.getInputStream());
+            if (!CollectionUtils.isEmpty(messages4)) {
+                for (String msg : messages4) {
+                    System.out.println(msg);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public static List<String> printMessage(final InputStream input) {
         List<String> messages = new ArrayList<>();
 
         Reader reader = new InputStreamReader(input);
