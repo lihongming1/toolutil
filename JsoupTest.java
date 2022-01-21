60b0324ea3342f37f7db2310a212cdcb9ed9545b
lihongming1 <45524483+lihongming1@users.noreply.github.com>
2022-01-21 15:04:52 +0800

    测试爬虫
    测试爬虫

--- JsoupTest.java
+++ JsoupTest.java
@@ -1,30 +1,52 @@
- implementation group: 'org.jsoup', name: 'jsoup', version: '1.14.3'
- implementation group: 'cn.wanghaomiao', name: 'JsoupXpath', version: '2.5.1'
-   
-     public static void main(String[] args) throws Exception {
-        File file = new File("/Users/sftc/Downloads/test.txt");
+package com.tblh.bms.util;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.seimicrawler.xpath.JXNode;
+
+import org.seimicrawler.xpath.JXDocument;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
+/**
+ * 测试爬虫
+ * <p>
+ * implementation group: 'org.jsoup', name: 'jsoup', version: '1.14.3'
+ * implementation group: 'cn.wanghaomiao', name: 'JsoupXpath', version: '2.5.1'
+ */
+public class JsoupTest {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("/Users/sftc/Downloads/test2.txt");
+        if (file.exists()) {
+            file.delete();
+        }
+        file.createNewFile();
         FileWriter fileWriter = new FileWriter(file, true);
-        for (int i = 129; i <= 1000; i++) {
-            String contents = Jsoup.connect("http://www.aibj.pro/forum-137-" + i + ".html").get().html();
-            JXDocument jxd = new JXDocument(contents);
+        for (int i = 1; i <= 2; i++) {
+            Document document = Jsoup.connect("http://www.aibj.pro/forum-137-" + i + ".html").get();
+            JXDocument jxd = JXDocument.create(document);
             List<JXNode> list = jxd.selN("//*[contains(@id, 'normalthread_')]/tr/th/a[3]");
             if (CollectionUtils.isNotEmpty(list)) {
-                for (JXNode node : list) {
+                for (JXNode jxNode : list) {
                     String area = null;
                     String httpUrl = null;
                     String text = null;
                     try {
-                        area = node.getElement().parentNode().childNode(5).childNode(1).childNode(0).toString();
+                        area = jxNode.asElement().parentNode().childNode(5).childNode(1).childNode(0).toString();
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                     try {
-                        httpUrl = "http://www.aibj.pro/" + node.getElement().attr("href");
+                        httpUrl = "http://www.aibj.pro/" + jxNode.asElement().attr("href");
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                     try {
-                        text = node.getElement().text();
+                        text = jxNode.asElement().text();
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
@@ -37,3 +59,5 @@
         }
         fileWriter.close();
     }
+
+}
